ad092a12a4942ec89eade6523295c3ebbefe62dc
Mohammad Shahrukh Khan <102281342+MohammadShahrukhKhan@users.noreply.github.com>
2022-10-20 11:59:42 +0530

    Add files via upload

---
+++ Javascript/JS7.2.js
@@ -0,0 +1,8 @@
+
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+}
+
+let rv = sum(1,2,3);
+ console.log(rv)
---
+++ Javascript/JS7.3.js
@@ -0,0 +1,16 @@
+
+function gtst(p,q){
+    let r = p-q;
+    return r;
+ }
+ 
+ let resu = gtst(6,5);
+  console.log(resu);
+ 
+  if(resu>0){
+    console.log(" p is greater ")
+ }
+ else{
+    console.log(" q is greater ")
+ }
+ 
